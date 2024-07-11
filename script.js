96a691042e0d23cba401788fee0362d1273617fa
ddr <mrdrifter123@outlook.com>
2024-07-11 10:11:30 +1000

    removed playbtn

--- script.js
+++ script.js
@@ -74,22 +74,17 @@ const playBtn = document.querySelector(".play-btn");
 
 const plrScore = document.querySelector(".plr-score");
 const cpuScore = document.querySelector(".cpu-score");
+const Rounds = 5
 
-playBtn.addEventListener("click", function(){
-    plrScore.textContent += " 0";
-    cpuScore.textContent += " 0";
-
-
-});
 
 rockBtn.addEventListener("click", function(){
-    playRound("rock", getComputerChoice());
+    let winner = playRound("rock", getComputerChoice());
 });
 
 paperBtn.addEventListener("click", function(){
-    playRound("paper", getComputerChoice());
+    let winner = playRound("paper", getComputerChoice());
 });
 
 scissorsBtn.addEventListener("click", function(){
-    playRound("scissors", getComputerChoice());
+    let winner = playRound("scissors", getComputerChoice());
 });
